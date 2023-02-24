fa999586967e7ce8ff1b1129681d163b9394031b
chamika-prasad <chamikap40@gmail.com>
2023-02-24 20:39:18 +0530

    uptaed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import HomePage from './pages/HomePage';
+import FilmList from './pages/FilmList';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<HomePage />}/>
+
+        <Route path='/FilmList' element={<FilmList />}/>
       </Routes>
     </div>
   );
